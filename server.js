fb8ac68c234a313d8dedb5ff0ba976781f9f55c0
rxtatx <rachel.thiim@gmail.com>
2020-07-21 19:40:00 -0500

    Converted to secret .env database and passport secret keys

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const passport = require("./config/passport");
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/MyDatabase',
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/${process.env.DATABASE}`,
   { useNewUrlParser: true, useUnifiedTopology: true })
 
 // Creating express app and configuring middleware needed for authentication
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
 }
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+  session({ secret: process.env.SECRET, resave: true, saveUninitialized: true })
 );
 app.use(passport.initialize());
 app.use(passport.session());
