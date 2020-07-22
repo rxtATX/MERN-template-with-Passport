647dfe2ee9c03b58e7bc3fa612eae9955d3c636b
rxtatx <rachel.thiim@gmail.com>
2020-07-21 19:40:42 -0500

    Converted to secret .env database and passport secret keys

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 }
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+  session({ secret: process.env.SECRET, resave: true, saveUninitialized: true })
 );
 app.use(passport.initialize());
 app.use(passport.session());
